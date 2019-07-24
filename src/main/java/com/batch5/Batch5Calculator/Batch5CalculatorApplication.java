03ad2da8e4a3c59c5e17204f24cb648d7b3501ad
SureshSc <scs.6190@gmail.com>
2019-07-24 12:26:36 +0530

    Boiler plate app

---
+++ src/main/java/com/batch5/Batch5Calculator/Batch5CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.batch5.Batch5Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch5CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch5CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch5/Batch5Calculator/Batch5CalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.batch5.Batch5Calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Batch5CalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
